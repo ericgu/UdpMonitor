2e00cdbcd62fa5ffc053afd34310dc66bbab5695
Eric Gunnerson <eric_gunnerson@hotmail.com>
2018-10-02 10:36:07 -0700

    Updated.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,9 @@
-﻿namespace UdpMonitor
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpMonitor
 {
     partial class Form1
     {
@@ -28,13 +33,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this._buttonClear = new System.Windows.Forms.Button();
+            this._listViewMessages = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // _buttonClear
+            // 
+            this._buttonClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._buttonClear.Location = new System.Drawing.Point(924, 12);
+            this._buttonClear.Name = "_buttonClear";
+            this._buttonClear.Size = new System.Drawing.Size(75, 23);
+            this._buttonClear.TabIndex = 1;
+            this._buttonClear.Text = "Clear";
+            this._buttonClear.UseVisualStyleBackColor = true;
+            this._buttonClear.Click += new System.EventHandler(this._buttonClear_Click);
+            // 
+            // _listViewMessages
+            // 
+            this._listViewMessages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._listViewMessages.Location = new System.Drawing.Point(12, 12);
+            this._listViewMessages.Name = "_listViewMessages";
+            this._listViewMessages.Size = new System.Drawing.Size(884, 557);
+            this._listViewMessages.TabIndex = 2;
+            this._listViewMessages.UseCompatibleStateImageBehavior = false;
+            this._listViewMessages.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1019, 581);
+            this.Controls.Add(this._listViewMessages);
+            this.Controls.Add(this._buttonClear);
+            this.Name = "Form1";
+            this.Text = "UDP Listener";
+            this.ResumeLayout(false);
+
         }
 
+
+
         #endregion
+        private System.Windows.Forms.Button _buttonClear;
+        private System.Windows.Forms.ListView _listViewMessages;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,60 @@ namespace UdpMonitor
 {
     public partial class Form1 : Form
     {
+        UdpClient _udpClient;
+
         public Form1()
         {
             InitializeComponent();
+            Load += Form1_Load;
+            _listViewMessages.Columns.Add("Time", 80, HorizontalAlignment.Left);
+            _listViewMessages.Columns.Add("IP", 120, HorizontalAlignment.Left);
+            _listViewMessages.Columns.Add("Message", 800, HorizontalAlignment.Left);
+        }
+
+        private void _buttonClear_Click(object sender, EventArgs e)
+        {
+            _listViewMessages.Items.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _udpClient = new UdpClient(12345);
+            StartListening();
+        }
+
+        IAsyncResult _asyncResult = null;
+
+        private void StartListening()
+        {
+            _asyncResult = _udpClient.BeginReceive(Receive, null);
+        }
+
+        private void Receive(IAsyncResult ar)
+        {
+            if (_listViewMessages.InvokeRequired)
+            {
+                BeginInvoke((Action)(() =>
+                {
+                    Receive(ar);
+                }));
+            }
+            else
+            {
+                IPEndPoint ip = new IPEndPoint(IPAddress.Any, 12344);
+                byte[] bytes = _udpClient.EndReceive(ar, ref ip);
+                string message = Encoding.ASCII.GetString(bytes);
+                ListViewItem listViewItem = new ListViewItem(new [] 
+                {
+                    DateTime.Now.ToLongTimeString(),
+                    ip.Address.ToString(),
+                    message });
+                _listViewMessages.Items.Add(listViewItem);
+                _listViewMessages.EnsureVisible(_listViewMessages.Items.Count - 1);
+
+                //Console.WriteLine("From {0} received: {1} ", ip.Address.ToString(), message);
+                StartListening();
+            }
         }
     }
 }
